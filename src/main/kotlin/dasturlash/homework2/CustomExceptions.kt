41827b00387a938dae38fa089256707a5e00cd2c
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-11-23 21:25:04 +0500

    Added: Custom exception InvalidAmount

--- src/main/kotlin/dasturlash/homework2/CustomExceptions.kt
+++ src/main/kotlin/dasturlash/homework2/CustomExceptions.kt
@@ -13,4 +13,4 @@ class UserLimitIsOverException(message: String = "Your limit is over, you have 1
 : RuntimeException(message)
 
 class AccountNotFoundException(message: String = "Account not found",  val statusCode: HttpStatus = HttpStatus.NOT_FOUND) : RuntimeException(message)
-class InvalidAmountException(message: String = "Invalid amount",  val statusCode: HttpStatus = HttpStatus.BAD_REQUEST)
+class InvalidAmountException(message: String = "Invalid amount",  val statusCode: HttpStatus = HttpStatus.BAD_REQUEST) : RuntimeException(message)
